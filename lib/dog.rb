c07591a63e9c791c8c016ae427c52f5c85560a89
Tanya Bihag <tbihag@gmail.com>
2019-09-01 02:38:50 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,8 @@
 class Dog 
   def initialize(dogs_name,dogs_breed)
     @name = dogs_name
-    @breed = (dogs_breed, "Mutt")
-    
+    @breed = dogs_breed
+    dogs_breed = "Mutt"
   end
-
-  
-
 end   
   
